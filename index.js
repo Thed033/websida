250a83ca772a3bc474ec38111c96aa799926a40c
Thed033 <102379353+Thed033@users.noreply.github.com>
2022-07-04 14:29:25 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function greet(name, lastName) {
+    console.log('Hello '+ name + ' ' + lastName)
+}
+
+greet('Thed', 'Molin');
+greet('joel', 'Molin'); 
+
+function sqare(number){
+    return number * number;
+}
+
+console.log(sqare(10));
